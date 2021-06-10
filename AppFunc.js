db9cb521967974ee987c70d5c3dc803931fdabf2
Varnius <akimirksnis@gmail.com>
2021-06-11 00:16:41 +0300

    add functional component

---
+++ AppFunc.js
@@ -0,0 +1,131 @@
+import React, {useEffect} from 'react'
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+
+import ArgyleSdk from '@argyleio/argyle-plugin-react-native'
+
+const TOKEN_KEY = 'userKey'
+
+const storeData = async token => {
+  try {
+    await AsyncStorage.setItem(TOKEN_KEY, token)
+  } catch (e) {
+    // saving error
+    console.log('error saving value', e)
+  }
+}
+
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem(TOKEN_KEY)
+    if (value !== null) {
+      // value previously stored
+    }
+    return value
+  } catch (e) {
+    // error reading value
+    console.log('error reading value', e)
+    return null
+  }
+}
+const App = () => {
+  useEffect(() => {
+    let token = ''
+    // const value = await getData()
+
+    // console.log('VALUE ', value)
+
+    // if (value !== null) {
+    //   token = value
+    // }
+
+    ArgyleSdk.loginWith('YOUR_PLUGIN_KEY', 'https://api-sandbox.argyle.io/v1', token)
+    ArgyleSdk.onUserCreated(res => {
+      console.log('onUserCreated', res)
+      storeData(res.token)
+    })
+
+    ArgyleSdk.onAccountCreated(res => console.log('onAccountCreated', res))
+    ArgyleSdk.onAccountConnected(res => console.log('onAccountConnected', res))
+    ArgyleSdk.onAccountUpdated(res => console.log('onAccountUpdated', res))
+    ArgyleSdk.onAccountRemoved(res => console.log('onAccountRemoved', res))
+    ArgyleSdk.onAccountError(res => console.log('onAccountError', res))
+    ArgyleSdk.onPayDistributionSuccess(res => console.log('onPayDistributionSuccess', res))
+    ArgyleSdk.onPayDistributionError(res => console.log('onPayDistributionError', res))
+    ArgyleSdk.onAccountError(res => console.log('onAccountError', res))
+    ArgyleSdk.onError(error => {
+      console.log('onError', error)
+      if (error === ArgyleSdk.errorCodes.EXPIRED_TOKEN) {
+        setTimeout(() => {
+          // Simulated timeout before updating the token
+          ArgyleSdk.updateToken('YOUR_NEW_TOKEN')
+        }, 300)
+      }
+    })
+    ArgyleSdk.onTokenExpired(res => console.log('onTokenExpired', res))
+
+    // ArgyleSdk.linkItems(["uber"])
+  }, [])
+
+  useEffect(() => {
+    /**
+     * Fired whenever the SDK closes regardless of user action.
+     **/
+    ArgyleSdk.onClose(() => {
+      console.log('close')
+    })
+    return () => {
+      ArgyleSdk.removeListenerIfAdded('onUserCreated')
+      ArgyleSdk.removeListenerIfAdded('onCantFindLinkItemClicked')
+      ArgyleSdk.removeListenerIfAdded('onAccountCreated')
+      ArgyleSdk.removeListenerIfAdded('onAccountConnected')
+      ArgyleSdk.removeListenerIfAdded('onAccountUpdated')
+      ArgyleSdk.removeListenerIfAdded('onAccountRemoved')
+      ArgyleSdk.removeListenerIfAdded('onAccountError')
+      ArgyleSdk.removeListenerIfAdded('onError')
+      ArgyleSdk.removeListenerIfAdded('onClose')
+    }
+  }, [ArgyleSdk])
+
+  const startNew = () => {
+    ArgyleSdk.updateToken('')
+    ArgyleSdk.start()
+  }
+
+  const startExisting = async () => {
+    ArgyleSdk.start()
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>📱️ Argyle Link SDK️</Text>
+      <TouchableOpacity onPress={startNew}>
+        <Text style={styles.welcome}>Start new</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={startExisting}>
+        <Text style={styles.welcome}>Start existing</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+})
+
+export default App
