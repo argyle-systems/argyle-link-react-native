313d235eaf90f02abeb0c3fc0eed20f459dc713b
Simonas Pauliukevičius <simonas@argyle.com>
2023-06-01 13:46:59 +0300

    Expose SDK wrapper and update example app (#53)
    * chore: expose SDK wrapper and update example app
    
    * Trigger auto QA tests using trigger-workflow-and-wait action
    
    ---------
    
    Co-authored-by: Viktoras Laukevičius <viktoras@argyle.com>

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  rules: {
-    'prettier/prettier': 'warn',
-    'semi': 0
-  }
-}
---
+++ example/json-demo-build.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import TestConfiguratorApp from './TestConfiguratorApp';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => TestConfiguratorApp);
