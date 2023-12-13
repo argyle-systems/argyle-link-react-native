648ca416637cd72388e9a9ad4b35a77699ce42e6
Varnius <simonas@argyle.com>
2023-12-14 00:50:32 +0200

    chore: bump deps

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import { LinkExample } from './src/LinkExample'
+import {LinkExample} from './src/LinkExample';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => LinkExample);
--- example/src/LinkExample.tsx
+++ example/src/LinkExample.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {
   AccountCallbackPayload,
