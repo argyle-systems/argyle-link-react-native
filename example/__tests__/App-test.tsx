a8df0e2994c5d1d911d028a42f3d2820abe45586
Simonas Pauliukevičius <simonas@argyle.com>
2023-12-21 13:29:44 +0200

    Revert "Bump RN to 0.73.1 (#93)" (#95)
    This reverts commit e80fd1b598e7cb428de1b6539fa9f177d332f198.

--- example/__tests__/App-test.tsx
+++ example/__tests__/App-test.tsx
@@ -4,14 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import TestConfiguratorApp from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<TestConfiguratorApp />);
 });
