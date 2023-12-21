e80fd1b598e7cb428de1b6539fa9f177d332f198
Simonas Pauliukevičius <simonas@argyle.com>
2023-12-21 13:14:49 +0200

    Bump RN to 0.73.1 (#93)

--- example/__tests__/App.test.tsx
+++ example/__tests__/App.test.tsx
@@ -4,11 +4,14 @@
 
 import 'react-native';
 import React from 'react';
-import TestConfiguratorApp from '../App';
+import App from '../App';
+
+// Note: import explicitly to use the types shipped with jest.
+import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<TestConfiguratorApp />);
+  renderer.create(<App />);
 });
