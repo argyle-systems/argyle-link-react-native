63b5c3df97600bc0b479d9ca8d74114bc0b197f3
Simonas <simonas@argyle.com>
2025-09-23 14:00:58 +0300

    fix Android build

--- example/index.js
+++ example/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import {AppRegistry} from 'react-native';
+import {LinkExample} from './src/LinkExample';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => LinkExample);
---
+++ example/src/LinkExample.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  AccountCallbackPayload,
+  ArgyleLink,
+  FormCallbackPayload,
+  Language,
+} from '@argyleio/argyle-plugin-react-native';
+
+export const LinkExample = (): JSX.Element => {
+  const callbackFired = (key: string, response: any = {}) => {
+    console.log('callback fired', key, response);
+  };
+
+  const start = () => {
+    const config = {
+      userToken: 'USER_TOKEN',
+      sandbox: true,
+      language: Language.EN,
+      onAccountCreated: (response: AccountCallbackPayload) =>
+        callbackFired('onAccountCreated', response),
+      onAccountConnected: (response: AccountCallbackPayload) =>
+        callbackFired('onAccountConnected', response),
+      onAccountRemoved: (response: AccountCallbackPayload) =>
+        callbackFired('onAccountRemoved', response),
+      onAccountError: (response: AccountCallbackPayload) =>
+        callbackFired('onAccountError', response),
+      onDDSSuccess: (response: AccountCallbackPayload) =>
+        callbackFired('onDDSSuccess', response),
+      onDDSError: (response: AccountCallbackPayload) =>
+        callbackFired('onDDSError', response),
+      onClose: () => callbackFired('onClose'),
+      onExitIntroClicked: () => callbackFired('onExitIntroClicked'),
+      onError: (response: any) => callbackFired('onError', response),
+      onTokenExpired: (updateToken: any) => {
+        // updateToken('<token>')
+        callbackFired('onTokenExpired', updateToken);
+      },
+      onUIEvent: (response: any) => callbackFired('onUIEvent', response),
+      onFormSubmitted: (response: FormCallbackPayload) =>
+        callbackFired('onFormSubmitted', response),
+      onDocumentsSubmitted: (response: FormCallbackPayload) =>
+        callbackFired('onDocumentsSubmitted', response),
+    };
+
+    ArgyleLink.start(config);
+  };
+
+  return (
+    <SafeAreaView style={styles.background}>
+      <Text style={styles.welcome}>📱️ Argyle Link SDK️</Text>
+      <TouchableOpacity testID="start" onPress={start}>
+        <Text style={styles.welcome}>Start</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {},
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
