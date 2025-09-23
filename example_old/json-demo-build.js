2e90da3ea68779e5ee4ffa2f4fe93270274d66e0
Simonas <simonas@argyle.com>
2025-09-23 15:19:29 +0300

    make iOS work

--- example_old/json-demo-build.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import TestConfiguratorApp from './TestConfiguratorApp';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => TestConfiguratorApp);
